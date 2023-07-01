1bf4217f814a74169ab5b0b02dd4b39556364c87
Anata Arisa <kakata28419@gmail.com>
2023-07-01 16:34:23 +0700

    Init

--- pim-tool/src/main/java/com/anataarisa/pimtool/repository/BaseRepository.java
+++ pim-tool/src/main/java/com/anataarisa/pimtool/repository/BaseRepository.java
@@ -1,7 +1,8 @@
 package com.anataarisa.pimtool.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BaseRepository<T> extends JpaRepository<T, Number>{
-    
+    public List<T> getAll();
 }
--- pim-tool/src/main/java/com/anataarisa/pimtool/repository/EmployeeRepository.java
+++ pim-tool/src/main/java/com/anataarisa/pimtool/repository/EmployeeRepository.java
@@ -1,6 +1,10 @@
 package com.anataarisa.pimtool.repository;
 
+import java.util.List;
+
 import com.anataarisa.pimtool.entity.Employee;
 
 public interface EmployeeRepository extends BaseRepository<Employee> {
+    public List<Employee> getAll();
+    public Employee savEmployee(Employee employee);
 }
